a7362ba122c5522e5a6203628c4377fc6ae2941c
echo <hujw77@gmail.com>
2021-01-26 21:32:37 +0800

    fnc: rm formulaInit

--- src/Formula.sol
+++ src/Formula.sol
@@ -6,18 +6,68 @@ import "./interfaces/IFormula.sol";
 import "./interfaces/ISettingsRegistry.sol";
 import "./FurnaceSettingIds.sol";
 
-contract Formula is Initializable, DSAuth, FurnaceSettingIds, IFormula {
+contract Formula is Initializable, DSAuth, IFormula {
 	event SetStrength(uint256 indexed inde, uint128 rate);
 
 	event SetAmount(uint256 indexed index, uint256 amount);
 
+	// 0x434f4e54524143545f4552433732315f4745474f000000000000000000000000
+	bytes32 public constant CONTRACT_ERC721_GEGO = "CONTRACT_ERC721_GEGO";
+
+	// 0x434f4e54524143545f4f424a4543545f4f574e45525348495000000000000000
+	bytes32 public constant CONTRACT_OBJECT_OWNERSHIP =
+		"CONTRACT_OBJECT_OWNERSHIP";
+
+	//0x434f4e54524143545f454c454d454e545f544f4b454e00000000000000000000
+	bytes32 public constant CONTRACT_ELEMENT_TOKEN = 
+		"CONTRACT_ELEMENT_TOKEN";
+
+	//0x434f4e54524143545f4c505f454c454d454e545f544f4b454e00000000000000
+	bytes32 public constant CONTRACT_LP_ELEMENT_TOKEN = 
+		"CONTRACT_LP_ELEMENT_TOKEN";
+
+	uint128 public constant RATE_DECIMALS = 10 ** 6;
+	uint256 public constant UNIT = 10 ** 18;
+
 	/*** STORAGE ***/
 
+	ISettingsRegistry public registry;
 	FormulaEntry[] public formulas;
 
-	function initialize() public initializer {
+	function initialize(address _registry) public initializer {
 		owner = msg.sender;
 		emit LogSetOwner(msg.sender);
+		registry = ISettingsRegistry(_registry);
+
+		_init();
+	}
+
+	function _init() internal {
+		address gego = registry.addressOf(CONTRACT_ERC721_GEGO); 
+		address ownership = registry.addressOf(CONTRACT_OBJECT_OWNERSHIP);
+		// 0
+		insert("合金镐", uint128(5 * RATE_DECIMALS), uint16(256), uint16(1), uint16(1), true, CONTRACT_ELEMENT_TOKEN, 500 * UNIT, gego, 256, 0, 1);
+
+		// 1
+		insert("人力铸铁钻机", uint128(5 * RATE_DECIMALS), uint16(4), uint16(1), uint16(1), true, CONTRACT_ELEMENT_TOKEN, 500 * UNIT, ownership, 4, 0, 1);
+
+		// 2
+		insert("人力钨钢钻机", uint128(12 * RATE_DECIMALS), uint16(4), uint16(1), uint16(2), true, CONTRACT_ELEMENT_TOKEN, 500 * UNIT, ownership, 4, 0, 2);
+
+		// 3
+		insert("人力金刚钻机", uint128(21 * RATE_DECIMALS), uint16(4), uint16(1), uint16(3), true, CONTRACT_ELEMENT_TOKEN, 500 * UNIT, ownership, 4, 0, 3);
+
+		// 4
+		insert("高级合金镐", uint128(28 * RATE_DECIMALS), uint16(256), uint16(2), uint16(1), true, CONTRACT_LP_ELEMENT_TOKEN, 450 * UNIT, ownership, 256, 1, 1);
+
+		// 5
+		insert("燃油铸铁钻机", uint128(28 * RATE_DECIMALS), uint16(4), uint16(2), uint16(1), true, CONTRACT_LP_ELEMENT_TOKEN, 450 * UNIT, ownership, 4, 1, 1);
+
+		// 6
+		insert("燃油钨钢钻机", uint128(68 * RATE_DECIMALS), uint16(4), uint16(2), uint16(2), true, CONTRACT_LP_ELEMENT_TOKEN, 450 * UNIT, ownership, 4, 1, 2);
+
+		// 7
+		insert("燃油金刚钻机", uint128(120 * RATE_DECIMALS), uint16(4), uint16(2), uint16(3), true, CONTRACT_LP_ELEMENT_TOKEN, 450 * UNIT, ownership, 4, 1, 3);
 	}
 
 	function insert(
@@ -33,7 +83,7 @@ contract Formula is Initializable, DSAuth, FurnaceSettingIds, IFormula {
 		uint16 _majorObjClassExt,
 		uint16 _majorClass,
 		uint16 _majorGrade
-	) external override auth {
+	) public override auth {
 		FormulaEntry memory formula =
 			FormulaEntry({
 				name: _name,
