74086a5ca6bc7c5000ab581bd480c70c18611c3e
echo <hujw77@gmail.com>
2021-02-24 18:58:19 +0800

    add pangolin network

--- fnc/default.nix
+++ fnc/default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {}
 , dappPkgs ? import (pkgs.fetchgit {
     url = "https://github.com/hujw77/dapptools";
-    rev = "seth/0.9.4";
+    rev = "seth/0.9.5";
     sha256 = "0c4h1fv81cw3dvlzw1748rv374q6xi3bdn95gv9bzkraqyadvwif";
     fetchSubmodules = true;
   }) {}
