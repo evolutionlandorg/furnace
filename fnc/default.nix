2220f243e53029d0d73e91d1f360e174c25f5c8c
echo <hujw77@gmail.com>
2021-04-24 11:04:16 +0800

    fnc 2.0.0

--- fnc/default.nix
+++ fnc/default.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {}
 , dappPkgs ? import (pkgs.fetchgit {
     url = "https://github.com/hujw77/dapptools";
-    rev = "seth/0.9.6";
-    sha256 = "0nvylcs7da6n3rbyvzlnrgajdw453k0a3f32ba37p0gc2wz8y9iy";
+    rev = "seth/0.10.3";
+    sha256 = "1zr4mjypzx9p1xrj22bprlp6lna3jjg990dqn3g1mh293brj4mig";
     fetchSubmodules = true;
   }) {}
 }:
