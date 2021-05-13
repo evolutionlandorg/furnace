f4fa109885d99f94c50ebb503d04d7c2d338edfa
echo <hujw77@gmail.com>
2021-05-13 19:20:15 +0800

    tron support

--- migrations/2_deploy_furnace.js
+++ migrations/2_deploy_furnace.js
@@ -6,8 +6,8 @@ var DrillTakeBack = artifacts.require("DrillTakeBack")
 var DrillLuckyBox = artifacts.require("DrillLuckyBox")
 var DrillBaseAuthority = artifacts.require("DrillBaseAuthority")
 var ObjectOwnershipAuthorityV4 = artifacts.require("ObjectOwnershipAuthorityV4")
-var ObjectOwnership = artifacts.require("ObjectOwnership")
-var SettingsRegistry = artifacts.require("SettingsRegistry")
+var LandResourceV6 = artifacts.require("LandResourceV6")
+var TronWeb = require('tronweb')
 
 const SETTINGSREGISTRY = {
   base58: "TV7XzfAcDrcpFmRkgCh2gg8wf15Cz9764W",
@@ -19,8 +19,25 @@ const Supervisor = {
   hex: "4100A1537D251A6A4C4EFFAB76948899061FEA47B9"
 }
 
+const LandResourceAuthority = {
+  base58: "TQCr6mPg4C3HDKFU72m34Vn8C3PLc3g4sN",
+  hex: "419c2622fc3074864a19bf9cc1d8e7b50eb60be31c"
+}
+
+const tronWeb = new TronWeb({
+  fullHost: 'https://api.shasta.trongrid.io',
+  headers: { "TRON-PRO-API-KEY": process.env.API_KEY },
+  privateKey: process.env.PRIVATE_KEY_SHASTA
+})
+
 let network;
 let timestamp = 1620804505;
+let resourceStartTime = 1579422612;
+let params = {
+  feeLimit:1000000000,
+  callValue: 1000,
+  shouldPollResponse:true
+}
 
 module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
@@ -34,6 +51,7 @@ async function asyncDeploy(deployer, network, accounts) {
   } else if (network == "mainnet") {
     network = 200000
   }
+  tronWeb.setDefaultBlock('latest');
   //---------------------deploy-------------------------//
   await deployer.deploy(DrillBase, SETTINGSREGISTRY.hex);
   let drillBase = await DrillBase.deployed();
@@ -48,25 +66,67 @@ async function asyncDeploy(deployer, network, accounts) {
   await deployer.deploy(DrillLuckyBox, SETTINGSREGISTRY.hex, drillTakeBack.address, timestamp);
   let drillLuckyBox = await DrillLuckyBox.deployed();
   //---------------------upgrade-------------------------//
-  
+  await deployer.deploy(LandResourceV6, SETTINGSREGISTRY.hex, resourceStartTime)
+  let landrs = await LandResourceV6.deployed()
+  await landrs.setAuthority(LandResourceAuthority.hex)
+  console.log("LandResourceV6 setAuthority succeed.")
   //---------------------auth-------------------------//
-  let settingsRegistry = SettingsRegistry.at(SETTINGSREGISTRY.hex);
-  let landBaseAddr = await settingsRegistry.addressOf("0x434f4e54524143545f4c414e445f424153450000000000000000000000000000")
-  console.log(landBaseAddr)
-  let apostleBaseAddr = await settingsRegistry.addressOf("0x434f4e54524143545f41504f53544c455f424153450000000000000000000000")
-  console.log(apostleBaseAddr)
-  deployer.deploy(DrillBaseAuthority, [drillTakeBack.address]);
+  let registry = await tronWeb.contract().at(SETTINGSREGISTRY.hex);
+  // CONTRACT_LAND_BASE
+  let landBaseAddr = await registry.addressOf("0x434f4e54524143545f4c414e445f424153450000000000000000000000000000").call()
+  // CONTRACT_APOSTLE_BASE
+  let apostleBaseAddr = await registry.addressOf("0x434f4e54524143545f41504f53544c455f424153450000000000000000000000").call()
+  await deployer.deploy(DrillBaseAuthority, [drillTakeBack.address]);
   let drillBaseAuthority = DrillBaseAuthority.deployed();
-  await drillBase.setAuthority(drillBaseAuthority.address);
-  deployer.deploy(ObjectOwnershipAuthorityV4, [landBaseAddr, apostleBaseAddr, drillBase.address, itemBase.address]);
+  await drillBase.setAuthority(DrillBaseAuthority.address);
+  console.log("DrillBase setAuthority succeed.")
+  await deployer.deploy(ObjectOwnershipAuthorityV4, [landBaseAddr, apostleBaseAddr, DrillBase.address, ItemBase.address]);
   let objectOwnershipAuthorityV4 = ObjectOwnershipAuthorityV4.deployed();
-  let objectOwnershipAddr = await settingsRegistry.addressOf("0x434f4e54524143545f4f424a4543545f4f574e45525348495000000000000000")
-  console.log(objectOwnershipAddr)
-  let objectOwnership = ObjectOwnership.at(OBJECTOWNERSHIP.hex);
-  await objectOwnership.setAuthority(objectOwnershipAuthorityV4.address);
+  // CONTRACT_OBJECT_OWNERSHIP
+  let objectOwnershipAddr = await registry.addressOf("0x434f4e54524143545f4f424a4543545f4f574e45525348495000000000000000").call()
+  let objectOwnership = await tronWeb.contract().at(objectOwnershipAddr);
+  await objectOwnership.setAuthority(ObjectOwnershipAuthorityV4.address);
+  console.log("ObjectOwnership setAuthority succeed.")
   //---------------------registry-------------------------//
-  let encoderAddr = await settingsRegistry.address("0x434f4e54524143545f494e5445525354454c4c41525f454e434f444552000000")
-  await encoder.registerNewObjectClass(drillBase.address, 4)
-  // TODO
+  // CONTRACT_INTERSTELLAR_ENCODER
+  let encoderAddr = await registry.addressOf("0x434f4e54524143545f494e5445525354454c4c41525f454e434f444552000000").call()
+  let encoder = await tronWeb.contract().at(encoderAddr); 
+  await encoder.registerNewObjectClass(DrillBase.address, 4).send(params);
+  console.log("encoder registerNewObjectClass DrillBase succeed.")
+  await encoder.registerNewObjectClass(ItemBase.address, 5).send(params);
+  console.log("encoder registerNewObjectClass ItemBase succeed.")
+  // CONTRACT_DRILL_BASE 
+  await registry.setAddressProperty("0x434f4e54524143545f4452494c4c5f4241534500000000000000000000000000", DrillBase.address).send(params)
+  console.log("registry CONTRACT_DRILL_BASE succeed.")
+  // CONTRACT_ITEM_BASE
+  await registry.setAddressProperty("0x434f4e54524143545f4954454d5f424153450000000000000000000000000000", ItemBase.address).send(params)
+  console.log("registry CONTRACT_ITEM_BASE succeed.")
+  // CONTRACT_FORMULA
+  await registry.setAddressProperty("0x434f4e54524143545f464f524d554c4100000000000000000000000000000000", Formula.address)
+  console.log("registry CONTRACT_FORMULA succeed.")
+  // CONTRACT_METADATA_TELLER
+  await registry.setAddressProperty("0x434f4e54524143545f4d455441444154415f54454c4c45520000000000000000", MetaDataTeller.address);
+  console.log("registry CONTRACT_METADATA_TELLER succeed.")
+  // CONTRACT_LP_GOLD_ERC20_TOKEN
+  // await registry.setAddressProperty("0x434f4e54524143545f4c505f474f4c445f45524332305f544f4b454e00000000", LP_GOLD.hex);
+  // CONTRACT_LP_WOOD_ERC20_TOKEN
+  // await registry.setAddressProperty("0x434f4e54524143545f4c505f574f4f445f45524332305f544f4b454e00000000", LP_WOOD.hex);
+  // CONTRACT_LP_WATER_ERC20_TOKEN
+  // await registry.setAddressProperty("0x434f4e54524143545f4c505f57415445525f45524332305f544f4b454e000000", LP_WATER.hex);
+  // CONTRACT_LP_FIRE_ERC20_TOKEN
+  // await registry.setAddressProperty("0x434f4e54524143545f4c505f464952455f45524332305f544f4b454e00000000", LP_FIRE.hex);
+  // CONTRACT_LP_SOIL_ERC20_TOKEN
+  // await registry.setAddressProperty("0x434f4e54524143545f4c505f534f494c5f45524332305f544f4b454e00000000", LP_SOIL.hex);
+  let drillId = "0x434f4e54524143545f4452494c4c5f4241534500000000000000000000000000"
+  await metaDataTeller.addInternalTokenMeta(drillId, 1, 1000000)
+  await metaDataTeller.addInternalTokenMeta(drillId, 2, 5000000)
+  await metaDataTeller.addInternalTokenMeta(drillId, 3, 12000000)
+  console.log("metaDataTeller addInternalTokenMeta succeed.")
+  // FURNACE_ITEM_MINE_FEE
+  await registry.setUintProperty("0x4655524e4143455f4954454d5f4d494e455f4645450000000000000000000000", 5000000).send(params)
+  console.log("registry FURNACE_ITEM_MINE_FEE succeed.")
+  // UINT_ITEMBAR_PROTECT_PERIOD
+  await registry.setUintProperty("0x55494e545f4954454d4241525f50524f544543545f504552494f440000000000", 604800).send(params)
+  console.log("registry UINT_ITEMBAR_PROTECT_PERIOD succeed.")
 }
 
